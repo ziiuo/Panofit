bca90a8e62a9d2cf94b286df64fc87b7ad60e5bd
“chenzhuo“ <15395643681@163.com>
2026-05-12 17:11:39 +0800

    deploy

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE = 'panofit-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(caches.keys().then((keys) =>
+    Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then((cached) => {
+      const fetched = fetch(e.request).then((res) => {
+        if (res.ok) {
+          const clone = res.clone();
+          caches.open(CACHE).then((c) => c.put(e.request, clone));
+        }
+        return res;
+      });
+      return cached || fetched;
+    })
+  );
+});
